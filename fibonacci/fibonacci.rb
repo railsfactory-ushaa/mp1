2f4fbadaed59a895bafadbb5267292aa889fd57a
usha <usha.ambig@gmail.com>
2014-07-02 16:22:04 +0530

    To print fibonacci series

---
+++ fibonacci/fibonacci.rb
@@ -0,0 +1,17 @@
+first =0
+second =1
+puts "Enter the number of terms\n"
+n=gets.to_i
+puts "First #{n} terms of Fibonacci series are :-\n"
+ 
+for c in 0..n
+   
+      if ( c <= 1 )
+         next1 = c
+      else
+         next1 = first + second
+         first = second
+         second = next1
+      end
+         printf "#{next1}\t"
+end
