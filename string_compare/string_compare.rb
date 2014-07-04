638c6b410669fb4bd97b94dbde8d8eb5c0adab2f
usha <ushaambig@railsfactory.org>
2014-07-04 16:41:06 +0530

    comparision of two strings

---
+++ string_compare/string_compare.rb
@@ -0,0 +1,13 @@
+a = Array.new(100)
+b = Array.new(100)
+printf "Enter the first string\n"
+a = gets.chomp
+printf("Enter the second string\n");
+b = gets.chomp
+ 
+if (a <=>b) == 0 
+   printf "Entered strings are equal.\n"
+else
+    printf "Entered strings are not equal.\n"
+end
+ 
