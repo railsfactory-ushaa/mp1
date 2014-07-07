c2bf3b0b10d53c620594abf808c903cd3ad42e1a
usha <ushaambig@railsfactory.org>
2014-07-07 18:22:39 +0530

    To generate floyds triangle

--- floyds_triangle/floyds_triangle.rb
+++ floyds_triangle/floyds_triangle.rb
@@ -1,11 +1,11 @@
-a =1
+a = 1
 puts "Enter the number of rows of Floyd's triangle to print\n"
 n = gets.to_i
 for i in 1..n
-   for c in 1..i
-   printf "#{a} "
-   a=a+1
-   end
-   printf "\n"
+  for c in 1..i
+    printf "#{a} "
+    a=a+1
+  end
+  printf "\n"
 end
  
