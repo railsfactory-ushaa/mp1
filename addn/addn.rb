d626a7abc45e04a5535a232707da7f7210e81280
usha <ushaambig@railsfactory.org>
2014-07-07 17:51:10 +0530

    To add given integers

--- addn/addn.rb
+++ addn/addn.rb
@@ -1,9 +1,9 @@
 puts "enter the  no. of integers you want to add"
-n=gets.to_i
-sum=0
-for c in 1..n 
+n = gets.to_i
+sum = 0
 puts "enter the integers"
-a=gets.to_i
-sum=sum+a
+for c in 1..n 
+  a = gets.to_i
+  sum = sum + a
 end
 puts "sum of entered no.s is: #{sum}"
