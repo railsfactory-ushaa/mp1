f6d34ffa8e8a4279b7bc05cf3e71a12664d56deb
usha <ushaambig@railsfactory.org>
2014-07-07 17:55:30 +0530

    To perform arithmatic operations

--- arithmatic/arithmatic.rb
+++ arithmatic/arithmatic.rb
@@ -1,13 +1,11 @@
 puts "enter two numbers"
-first=gets
-second=gets
-f=first.to_i
-s=second.to_i
-add=f+s
-subtract=f-s
-multiply=f*s
-divide=f/s
-puts "sum= #{add}"
-puts "difference= #{subtract}"
-puts "multiplication= #{multiply}"
-puts "division= #{divide}"
+first = gets.to_i
+second = gets.to_i
+add = first + second
+subtract = first - second
+multiply = first * second
+divide = first.to_f / second.to_f
+puts "sum = #{add}"
+puts "difference = #{subtract}"
+puts "multiplication = #{multiply}"
+puts "division = #{divide}"
