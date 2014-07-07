2f46a0a71065867edf1e63130023dfa772d64e96
usha <ushaambig@railsfactory.org>
2014-07-07 18:25:35 +0530

    to generate armstrong number

--- generate_armstrong_number/gen_armstrong_no.rb
+++ generate_armstrong_number/gen_armstrong_no.rb
@@ -1,18 +1,17 @@
-number=0
-sum=0
+number = 0
+sum = 0
 puts "Enter an integer upto which you want to find armstrong numbers\n"
-number=gets.to_i
+number = gets.to_i
 printf "Following armstrong numbers are found from 1 to #{number}\n"
 for c in 1..number
-    temp = c
-    while( temp != 0 ) do
-       r = temp%10
-       sum = sum + r*r*r
-       temp = temp/10
-    end
-      
-    if ( c == sum )
-       printf "#{c}\n"
-    end
-    sum = 0
+  temp = c
+  while  temp != 0  do
+    r = temp%10
+    sum = sum + r*r*r
+    temp = temp/10
+  end
+  if  c == sum 
+    printf "#{c}\n"
+  end
+  sum = 0
 end
