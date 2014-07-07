3efd06ec6ad49312dbddd407cd72d097f1f5f850
usha <ushaambig@railsfactory.org>
2014-07-07 16:59:29 +0530

    To add 2 complex numbers

---
+++ add_complex/add_complex.rb
@@ -0,0 +1,10 @@
+puts "enter real and imaginary parts of first complex number"
+n1 = gets.chomp
+m1 = gets.chomp
+puts "enter real and imaginary parts of second complex number"
+n2 = gets.chomp
+m2 = gets.chomp
+complex = Complex(n1,m1) + Complex(n2,m2)
+puts complex
+
+
