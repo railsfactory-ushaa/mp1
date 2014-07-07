2e7c8932fcd559f467f3a71edaae85da21052108
usha <ushaambig@railsfactory.org>
2014-07-07 18:20:25 +0530

    To generate fibonacci series

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,17 +1,16 @@
-first =0
-second =1
+first = 0
+second = 1
 puts "Enter the number of terms\n"
-n=gets.to_i
+n = gets.to_i
 puts "First #{n} terms of Fibonacci series are :-\n"
  
 for c in 0..n
-   
-      if ( c <= 1 )
-         next1 = c
-      else
-         next1 = first + second
-         first = second
-         second = next1
-      end
-         printf "#{next1}\t"
+  if  c <= 1 
+    next1 = c
+  else
+    next1 = first + second
+    first = second
+    second = next1
+  end
+  printf "#{next1}\t"
 end
