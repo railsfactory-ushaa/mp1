94138809f5431a70b4c938666273e2ab1649a47d
usha <ushaambig@railsfactory.org>
2014-07-07 16:30:42 +0530

    To delete a file

---
+++ delete_file/delete_file.rb
@@ -0,0 +1,8 @@
+puts "Enter the name of file you wish to delete"
+file_name = gets.chomp
+ 
+status = File.delete(file_name)
+  
+  if( status == 0 )
+      puts "#{file_name} file deleted successfully."
+  end
