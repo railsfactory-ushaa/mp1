5b70d865f9ca3bc17e7fee88ce5423b34f0fa62e
usha <ushaambig@railsfactory.org>
2014-07-07 17:58:53 +0530

    To check whether the given number is armstrong number or not

--- armstrong_number/armstrong.rb
+++ armstrong_number/armstrong.rb
@@ -1,12 +1,12 @@
-sum=0
+sum = 0
 puts "Enter an integer\n"
 number = gets.to_i
 temp = number
  
 while( temp != 0 ) do
-     remainder = temp%10;
-     sum = sum + remainder*remainder*remainder;
-     temp = temp/10;
+     remainder = temp % 10
+     sum = sum + remainder * remainder * remainder
+     temp = temp / 10
 end
 if ( number == sum )
    printf "Entered number is an armstrong number.\n" 
