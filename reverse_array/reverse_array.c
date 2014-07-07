48aff6ee959687ecb2cba0807dfbf4cf6823319a
usha <ushaambig@railsfactory.org>
2014-07-07 15:58:57 +0530

    To reverse the elements of the given array

---
+++ reverse_array/reverse_array.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+ 
+int main()
+{
+   int n, c, d, a[100], b[100];
+ 
+ 
+   return 0;
+}
---
+++ reverse_array/reverse_array.rb
@@ -0,0 +1,15 @@
+puts "enter the number of elements in the array"
+num = gets.chomp.to_i
+ arr = []
+ arr1 = []
+puts "enter the elemens of array one by one "
+  for i in 1..num
+  arr[i] = gets.chomp.to_i
+  end
+ puts "the array in reverse order is "
+  for i in 1..num
+  arr1 [i] = arr[num]
+  num = num -1
+
+  puts arr1 [i]
+  end
