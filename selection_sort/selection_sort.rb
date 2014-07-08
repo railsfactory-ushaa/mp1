149a6a3f4a8b5657240e18b98905efee58795647
usha <ushaambig@railsfactory.org>
2014-07-08 10:27:04 +0530

    To sort the elements of the array using selection sort

--- selection_sort/selection_sort.rb
+++ selection_sort/selection_sort.rb
@@ -3,28 +3,22 @@ printf "Enter number of elements\n"
 n = gets.to_i
 printf "Enter #{n} integers\n"
 for c in 0...n
-array[c] = gets.to_i
+  array[c] = gets.to_i
 end
-for c in 0..(n-1)
-   
-      position = c
- 
-      for  d in (c+1)...n
-     
-         if  array[position] > array[d] 
-            position = d
-         end
-     end
-      if  position != c 
-      
-         swap = array[c]
-         array[c] = array[position]
-         array[position] = swap
-      
-     end
+for c in 0..(n - 1)
+  position = c
+  for  d in (c + 1)...n
+    if  array[position] > array[d] 
+      position = d
+    end
+  end
+  if  position != c 
+    swap = array[c]
+    array[c] = array[position]
+    array[position] = swap
+  end
 end
- 
 printf "Sorted list in ascending order:\n"
 for c in 0..n
-printf "#{array[c]}\n"
+  printf "#{array[c]}\n"
 end
