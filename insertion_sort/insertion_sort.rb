c765bb24081d1efcdb45df8b8d52f14dd02cecf8
usha <ushaambig@railsfactory.org>
2014-07-08 09:41:14 +0530

    To sort the elements of the array using insertion sort

--- insertion_sort/insertion_sort.rb
+++ insertion_sort/insertion_sort.rb
@@ -6,19 +6,19 @@ for c in 0...n
   array[c] = gets.to_i
 end
  
-for c in 1..(n-1)
+for c in 1..(n - 1)
     d = c
-    while (d > 0 && array[d] < array[d-1]) do
+    while (d > 0 && array[d] < array[d - 1]) do
       t          = array[d]
-      array[d]   = array[d-1]
-      array[d-1] = t
-      d = d-1
+      array[d]   = array[d - 1]
+      array[d - 1] = t
+      d = d - 1
     end
 end
  
  
 printf "Sorted list in ascending order:\n"
-for c in 0..(n-1)
+for c in 0..(n - 1)
 printf "#{array[c]}\n"
 end
   
