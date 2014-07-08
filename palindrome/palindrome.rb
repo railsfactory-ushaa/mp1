5e0907f017822ffc061ad7ec0d38a8d47e991e9c
usha <ushaambig@railsfactory.org>
2014-07-08 10:04:44 +0530

    To check whether the given number is palindrome or not

--- palindrome/palindrome.rb
+++ palindrome/palindrome.rb
@@ -1,15 +1,15 @@
-reverse=0
+reverse = 0
 puts "Enter a number to check if it is a palindrome or not\n"
-n=gets.to_i
+n = gets.to_i
 temp = n
-while( temp != 0 )
-   reverse = reverse * 10;
-   reverse = reverse + temp%10;
-   temp = temp/10;
+while temp != 0 
+   reverse = reverse * 10
+   reverse = reverse + temp % 10
+   temp = temp / 10
 end
-if ( n == reverse )
-   printf "#{n} is a palindrome number.\n"
+if  n == reverse 
+  printf "#{n} is a palindrome number.\n"
 else
-   printf "#{n} is not a palindrome number.\n"
+  printf "#{n} is not a palindrome number.\n"
 end 
    
