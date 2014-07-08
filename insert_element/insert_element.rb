33e03d5534f5e3c3f516d1bdfc86c4700a8e0a3f
usha <ushaambig@railsfactory.org>
2014-07-08 09:38:18 +0530

    To insert element into the given array in the specified location

--- insert_element/insert_element.rb
+++ insert_element/insert_element.rb
@@ -1,21 +1,23 @@
 array = Array.new(100)
 printf "Enter number of elements in array\n"
-n=gets.to_i
+n = gets.to_i
 printf "Enter #{n} elements\n"
+
 for c in 0...n
-array[c] = gets.to_i
+  array[c] = gets.to_i
 end 
+
 printf "Enter the location where you wish to insert an element\n"
 position = gets.to_i
 printf "Enter the value to insert\n"
 value = gets.to_i 
-for c in (n-1)..(position-1)
-   array[c+1] = array[c]
+for c in (n - 1)..(position - 1)
+   array[c +1 ] = array[c]
 end
-   array[position-1] = value
-   printf "Resultant array is\n"
 
- 
+array[position - 1] = value
+printf "Resultant array is\n"
+
 for c in 0..n
    puts "#{array[c]}"
 end
