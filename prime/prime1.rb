7f983bfeb83554a1bcf394d58b59d6c77fd6f67a
usha <ushaambig@railsfactory.org>
2014-07-08 10:13:19 +0530

    To check whether a given number is prime or not

--- prime/prime1.rb
+++ prime/prime1.rb
@@ -1,19 +1,18 @@
 puts "enter a number"
-a=gets.chomp
-n=Integer(a)
-x=true
+a = gets.to_i
+x = true
 if n == 2
-puts "prime number"
+  puts "prime number"
 else
-for i in 2..(n-1)
-if ( n % i == 0)
-x=false
+for i in 2..(n - 1)
+  if  n % i == 0
+    x = false
+  end
 end
 end
-end
-if(x==true)
-puts "prime number"
+if x == true
+  puts "prime number"
 else
-puts "#{n} is not prime"
+  puts "#{n} is not prime"
 end
 
