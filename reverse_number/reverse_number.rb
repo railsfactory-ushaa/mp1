a36a2e4138d1b8c5eaef696a1937110cd5e21ac3
usha <ushaambig@railsfactory.org>
2014-07-08 10:21:00 +0530

    To reverse the given number

--- reverse_number/reverse_number.rb
+++ reverse_number/reverse_number.rb
@@ -1,10 +1,10 @@
-reverse=0
+reverse = 0
 puts "Enter a number to reverse\n"
 n = gets.to_i 
-while (n != 0) do
- reverse = reverse * 10
- reverse = reverse + n%10
- n = n/10
+while n != 0 do
+  reverse = reverse * 10
+  reverse = reverse + n % 10
+  n = n / 10
 end
 puts "Reverse of entered number is  #{reverse}"
  
