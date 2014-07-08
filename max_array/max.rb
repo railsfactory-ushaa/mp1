d72aedff0e3852568763934b706e4c43459f949f
usha <ushaambig@railsfactory.org>
2014-07-08 09:54:13 +0530

    To display the maximum element of the array

--- max_array/max.rb
+++ max_array/max.rb
@@ -1,4 +1,4 @@
-location=1
+location = 1
 array = Array.new(100)
 printf "Enter the number of elements in array\n"
 size = gets.to_i
@@ -8,12 +8,10 @@ for c in 0...size
 end 
 maximum = array[0]
 for c in 1...size
-  
-    if array[c] > maximum
-    
-       maximum  = array[c]
-       location = c+1
-    end
+  if array[c] > maximum
+    maximum  = array[c]
+    location = c + 1
+  end
 end
 printf "Maximum element is present at location #{location},  and it's value is #{maximum}\n"
 
