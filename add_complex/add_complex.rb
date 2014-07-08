5fe6ced74518f53621d8010b6a1fd39825312d33
usha <ushaambig@railsfactory.org>
2014-07-08 10:44:58 +0530

    To add 2 complex numbers

--- add_complex/add_complex.rb
+++ add_complex/add_complex.rb
@@ -1,10 +1,24 @@
-puts "enter real and imaginary parts of first complex number"
-n1 = gets.chomp
-m1 = gets.chomp
-puts "enter real and imaginary parts of second complex number"
-n2 = gets.chomp
-m2 = gets.chomp
-complex = Complex(n1,m1) + Complex(n2,m2)
-puts complex
+complex = Struct.new(:real,:img)
+ob = complex.new
+ob1 = complex.new
+ob2 = complex.new
+puts "enter a and b where a+ib is the first complex number"
+print "a="
+ob.real= gets.chomp.to_i
+print "b="
+ob.img = gets.chomp.to_i
+obj = complex.new("a","b")
+puts "enter c and d where c+id is the second complex number"
+print "c="
+ob1.real = gets.chomp.to_i
+print "d="
+ob1.img = gets.chomp.to_i
 
+ob2.real = ob.real+ob1.real
+ob2.img = ob.img + ob1.img
+if ob2.img > 0
+  puts "sum of two complex numbers is #{ob2.real}+#{ob2.img}i"
+else
+  puts "sum of two complex numbers is #{ob2.real}#{ob2.img}i"
+end
 
