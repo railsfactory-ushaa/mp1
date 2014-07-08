e848085c0366cf634ab693add19aef27982206e4
usha <ushaambig@railsfactory.org>
2014-07-08 09:46:54 +0530

    To search the element in the given array

--- linear_search/linear_search.rb
+++ linear_search/linear_search.rb
@@ -1,19 +1,19 @@
-array= Array.new(100)
+array = Array.new(100)
 printf "Enter the number of elements in array\n"
-n=gets.to_i
+n = gets.to_i
 printf "Enter #{n} integer(s)\n"
 for c in 0...n
   array[c] = gets.to_i
 end
 printf "Enter the number to search\n"
-search =gets.to_i
+search = gets.to_i
 for c in 0...n
- if (array[c] == search)    # if required element found
-  printf "#{search} is present at location #{c}\n"
-  break
- end
+  if array[c] == search    # if required element found
+    printf "#{search} is present at location #{c}\n"
+    break
+  end
 end     
-if (c == n)
-printf "#{search} is not present in array.\n"
+if c == n
+  printf "#{search} is not present in array.\n"
 end
  
