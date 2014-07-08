a031040a8fd4d14f6c6d8d12aa813ab794a06950
usha <ushaambig@railsfactory.org>
2014-07-08 10:16:42 +0530

    To read the contents of a file

--- read_file/read_file.rb
+++ read_file/read_file.rb
@@ -2,8 +2,8 @@ printf "Enter the name of file you wish to see\n"
 filename = gets.chomp
 file = File.open(filename, "r")
 puts "contents are :"
-while (line = file.gets) do
-puts " #{line}"
+while line = file.gets do
+  puts " #{line}"
 end
 
 
